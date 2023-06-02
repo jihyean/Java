4bcdcfde5512d57c5b72e02ca431a640017f81d9
jihyean <80264075+jihyean@users.noreply.github.com>
2023-06-02 17:27:48 +0900

    Add files via upload

---
+++ HW04/client/Client.java
@@ -0,0 +1,11 @@
+package client;
+
+import controller.Controller;
+
+public class Client {
+	public static void main(String[] args) {
+		Controller ctrl = new Controller();
+
+		ctrl.startApp();
+	}
+}
